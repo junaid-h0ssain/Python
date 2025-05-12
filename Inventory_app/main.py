d90f793683bf1115927571d87c1e689d33498f68
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-12 22:22:33 +0600

    made changes in inventory app

--- Inventory_app/main.py
+++ Inventory_app/main.py
@@ -1,10 +1,40 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 app = QApplication([])
 window = QWidget()
-window.setWindowTitle('Hello Worlds')
-window.resize(500,500)
+
+window.setWindowTitle('Inventory App')
+window.resize(500,400)
+
+title = QLabel('Inventory App')
+# addinfo = QLabel('?')
+# deleteinfo = QLabel('?')
+# viewinfo = QLabel('?')
+
+addbutton = QPushButton('Add')
+deletebutton = QPushButton('Delete/Remove')
+viewbutton = QPushButton('View/List Items')
+
+masterlayout = QVBoxLayout()
+row1 = QHBoxLayout()
+row2 = QHBoxLayout()
+row3 = QHBoxLayout()
+
+row1.addWidget(title,alignment=Qt.AlignCenter)
+# row2.addWidget(addinfo,alignment=Qt.AlignCenter)
+# row2.addWidget(deleteinfo,alignment=Qt.AlignCenter)
+# row2.addWidget(viewinfo,alignment=Qt.AlignCenter)
+row3.addWidget(addbutton)
+row3.addWidget(deletebutton)
+row3.addWidget(viewbutton)
+
+masterlayout.addLayout(row1)
+# masterlayout.addLayout(row2)
+masterlayout.addLayout(row3)
+
+window.setLayout(masterlayout)
+
 
 window.show()
 app.exec_()
