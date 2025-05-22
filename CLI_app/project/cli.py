98b696fa14d7df944946bce7dcb375f6fc2cd027
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-22 18:23:25 +0600

    cli app changes

--- CLI_app/project/cli.py
+++ CLI_app/project/cli.py
@@ -7,16 +7,23 @@ app = typer.Typer()
 
 @app.command()
 def add(tn: str,tp: str):
+    tid = 0
     tasks=[]
+    if os.path.exists(db):
+        with open(db,"r") as f:
+            tasks=json.load(f)
+            for task in tasks:
+                id = task['task_id']
+                tid =id
+                #print(id)
+    tid +=1
     task = {
+        'task_id':tid,
         "task_name":tn,
         "task_progress":tp
     }
     
-    if os.path.exists(db):
-        with open(db,"r") as f:
-            tasks=json.load(f)
-
+    
     tasks.append(task)
 
     with open(db, "w") as f:
@@ -42,7 +49,7 @@ def list():
 
     for task in tasks:
         typer.secho(
-            f"{tasks.index(task)+1}{(len(columns[0]) - len(str(tasks.index(task)))-1) * ' '}"
+            f"{task['task_id']}{(len(columns[0]) - len(str(tasks.index(task)))-1) * ' '}"
             f" | {task['task_name']}{(len(columns[1]) - len(str(task['task_name']))-3) * ' '}"
             f" | {task['task_progress']}",#"{(len(columns[2]) - len(str(task['task_progress']))) * ' '}",
             fg=typer.colors.GREEN,
@@ -50,6 +57,46 @@ def list():
         )
     typer.secho("-" * len(headers), fg=typer.colors.BRIGHT_RED, bold=True)
 
+@app.command()
+def update(tid: int,tn: str,tp : str):
+    tasks=[]
+    if os.path.exists(db):
+        with open(db,"r") as f:
+            tasks=json.load(f)
+
+    for task in tasks:
+        if tid == task['task_id']:
+            tasks.remove(task)
+            task = {
+                'task_id':tid,
+                "task_name":tn,
+                "task_progress":tp
+            }
+            tasks.append(task)
+            print(f"Task '{task}' updated to {db}.")
+            break
+
+    with open(db, "w") as f:
+        json.dump(tasks, f,indent=4)
+
+
+@app.command()
+def delete(tid: int):
+    tasks=[]
+    if os.path.exists(db):
+        with open(db,"r") as f:
+            tasks=json.load(f)
+
+    for task in tasks:
+        if tid == task['task_id']:
+            tasks.remove(task)
+            print(f"Task '{task}' deleted from {db}.")
+            break
+
+    with open(db, "w") as f:
+        json.dump(tasks, f,indent=4)
+
 
 if __name__ == "__main__":
     app()
+    id = 0
